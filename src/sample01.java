a835f22661f0b6d67e04baeb7b40615f4f6b6ab0
hayakawa1234 <61731486+hayakawa1234@users.noreply.github.com>
2020-03-06 17:59:55 +0900

    新しい演習　北ソフト　練習問題3-11

---
+++ src/sample01.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+
+/**
+ * @author hayak
+ *
+ */
+public class sample01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int num = 1;
+		
+		switch (num) {
+			case 1:
+				System.out.println("1Œ: Œ³“úA¬l‚Ì“ú");
+				break;
+			case 2:
+				System.out.println("2Œ: Œš‘‹L”O“ú");
+				break;
+			case 3:
+				System.out.println("3Œ: t•ª‚Ì“ú");
+				break;
+			case 4:
+				System.out.println("4Œ: º˜a‚Ì“ú");
+			    break;
+			case 5:
+				System.out.println("5Œ: Œ›–@‹L”O“úA‚İ‚Ç‚è‚Ì“úAq‚Ç‚à‚Ì“ú");
+				break;
+			case 6:
+				System.out.println("7Œ: ŠC‚Ì“ú");
+				break;
+			case 7:
+				System.out.println("9Œ: Œh˜V‚Ì“úAH•ª‚Ì“ú");
+				break;
+			case 8:
+				System.out.println("10Œ: ‘Ìˆç‚Ì“ú");
+				break;
+			case 9:
+				System.out.println("11Œ: •¶‰»‚Ì“úA‹Î˜JŠ´Ó‚Ì“ú");
+				break;
+			case 10:
+				System.out.println("12Œ: “Vc’a¶“ú");
+				break;
+					
+		}
+
+	}
+
+}
