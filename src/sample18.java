51310cc798f9222b661f4fac90a118ac68394b0b
hayakawa1234 <61731486+hayakawa1234@users.noreply.github.com>
2020-03-16 16:29:31 +0900

    新しい演習 北ソフト練習問題5-5

--- src/sample18.java
+++ src/sample18.java
@@ -1,50 +1,56 @@
 import java.io.*;
 
 /*
+ * 〇練習問題 5 - 5
  * 次のコードに追加し、入力された数値を 16 桁の２進数で表示するプログラムを完成させなさい。
  * 
  * BufferedReader br = new BufferedReader(
  * 
- * 						 new InputStreamReader( System.in ) );
+ * 		new InputStreamReader( System.in ) );
  * 
- * 							int value = Integer.parseInt( br.readLine() );
+ * 		int value = Integer.parseInt( br.readLine() );
  * 
- * 							int binary[] = new int[16];
+ * 		int binary[] = new int[16];
  * 
- * 							// 配列 binaryに0か1を代入する
+ * 		// 配列 binaryに0か1を代入する
  * 
- * for( int i = 0 ; i < 16 ; i++ )
- * System.out.print( binary[i] );
+ * 			for( int i = 0 ; i < 16 ; i++ )
+ *			 System.out.print( binary[i] );
  */
 
 public class sample18 {
 
 	public static void main(String[] args) {
 
-		
+		int value=0;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			System.out.println("10進数を入力してください");
-			int value = Integer.parseInt(br.readLine());
-		}
-
-		catch (IOException e) {
+			value = Integer.parseInt(br.readLine());
+		} catch (IOException e) {
 			System.out.println(e);
 			return;
-		}
-
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			System.out.println("Error: 数字を入力してください!");
 			return;
 		}
 
+		// 150 -> 0x0000000010010110
 		int binary[] = new int[16];
+		for (int i = 16-1; i >=0; i--) {
+			int pow = 1;
+			for(int j=0;j<i;j++) {
+				pow *= 2;
+			}
+			binary[i] = 0;
+			if(value>=pow) {
+				binary[i] = 1;				
+				value -= pow;
+			}
+		}
 
-//配列 binaryに0か1を代入する
-
-		for (int i = 0; i < 16; i++)
-				System.out.print(binary[i]);
-
+		for (int i = 15; i >=0; i--) {
+			System.out.print(binary[i]);
+		}
 	}
-
 }
---
+++ src/sample19.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class sample19 {
+
+	public static void main(String[] args) {
+
+	}
+}
